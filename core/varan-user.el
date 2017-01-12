c22f8c4d5bc56156a6c85e8e826d582e7dc7684f
skylerlee <skyler.ac.lee@gmail.com>
2017-01-12 23:32:06 +0800

    Add smex dependent package

--- core/varan-user.el
+++ core/varan-user.el
@@ -11,7 +11,8 @@
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default))))
+    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+ '(package-selected-packages (quote (smex monokai-theme magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
