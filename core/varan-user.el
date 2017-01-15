9b3bc960dd8879ce3379e32e934e6ee1a032b592
skylerlee <skyler.ac.lee@gmail.com>
2017-01-15 23:19:13 +0800

    Set font face preference

--- core/varan-user.el
+++ core/varan-user.el
@@ -18,4 +18,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 128 :width normal)))))
