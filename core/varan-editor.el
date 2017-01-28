f985519f89f54a21a239f23836d877410465efe0
skylerlee <skyler.ac.lee@gmail.com>
2017-01-28 22:08:27 +0800

    Update editor config

--- core/varan-editor.el
+++ core/varan-editor.el
@@ -3,9 +3,15 @@
 ;; Copyright (C) 2016, Skyler.
 ;; MIT License.
 
-;; Custom editor preferences
+;; Tweak editor preferences
 (setq-default cursor-type 'bar)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
+
+;; Tweak editing behavior
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 (provide 'varan-editor)
