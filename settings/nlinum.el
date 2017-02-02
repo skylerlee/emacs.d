154f8704420271e642a2a016c1b75fabe16e59b7
skylerlee <skyler.ac.lee@gmail.com>
2017-02-02 22:41:56 +0800

    Update nlinum mode settings

--- settings/nlinum.el
+++ settings/nlinum.el
@@ -1,3 +1,7 @@
 (use-package nlinum
+  :init
+  (setq nlinum-format "%4d")
   :config
-  (global-nlinum-mode t))
+  (progn
+    (add-hook 'prog-mode-hook 'nlinum-mode)
+    (add-hook 'text-mode-hook 'nlinum-mode)))
