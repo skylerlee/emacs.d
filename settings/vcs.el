cedfca74dfcc550b4c4f0ba0e60488d84a711b19
skylerlee <skyler.ac.lee@gmail.com>
2017-02-18 23:39:37 +0800

    Add magit package

--- settings/vcs.el
+++ settings/vcs.el
@@ -1,5 +1,8 @@
+(use-package magit)
+
 (use-package diff-hl
   :init
   (progn
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
     (global-diff-hl-mode)))
