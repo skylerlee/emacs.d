573f39eba94ce9d36a72a39e570e7aaef7136de1
skylerlee <skyler.ac.lee@gmail.com>
2017-02-19 17:15:46 +0800

    Enable diff highlight on the fly

--- settings/vcs.el
+++ settings/vcs.el
@@ -4,5 +4,6 @@
   :init
   (progn
     (add-hook 'after-init-hook 'global-diff-hl-mode)
+    (add-hook 'after-init-hook 'diff-hl-flydiff-mode)
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)))
