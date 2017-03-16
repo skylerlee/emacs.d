e00afd5363ec3c12dc2c5b0114667a3d104a6bd4
skylerlee <skyler.ac.lee@gmail.com>
2017-03-16 22:04:30 +0800

    Add nyan-mode

--- settings/misc.el
+++ settings/misc.el
@@ -4,3 +4,9 @@
     (setq nlinum-format "%4d")
     (add-hook 'prog-mode-hook 'nlinum-mode)
     (add-hook 'text-mode-hook 'nlinum-mode)))
+
+(use-package nyan-mode
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'nyan-mode)
+    (add-hook 'text-mode-hook 'nyan-mode)))
