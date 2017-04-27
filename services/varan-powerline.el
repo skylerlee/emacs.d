c436a8a0019667dafcf9a0d544b9728b014e8043
skylerlee <skyler.ac.lee@gmail.com>
2017-04-28 00:39:26 +0800

    Add powerline separator segment

--- services/varan-powerline.el
+++ services/varan-powerline.el
@@ -3,6 +3,22 @@
 ;; Copyright (C) 2016, Skyler.
 ;; MIT License.
 
+(require 'powerline)
+
+(setq powerline-default-separator 'slant)
+
+(defalias
+  'varan//pl-sep-left
+  (symbol-function (intern (format "powerline-%s-%s"
+                                   (powerline-current-separator)
+                                   (car powerline-default-separator-dir)))))
+
+(defalias
+  'varan//pl-sep-right
+  (symbol-function (intern (format "powerline-%s-%s"
+                                   (powerline-current-separator)
+                                   (cdr powerline-default-separator-dir)))))
+
 ;;;###autoload
 (defun varan/powerline-theme ()
   (interactive)
