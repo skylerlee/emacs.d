ee440c4482a56a81986ced2c176850ab15a35f4d
skylerlee <skyler.ac.lee@gmail.com>
2017-05-29 22:31:07 +0800

    Make cache directory hidden

--- core/bootstrap.el
+++ core/bootstrap.el
@@ -7,7 +7,7 @@
 
 (require 'varan-common)
 
-(varan|define-directory "cache" "Directory storing cache files." :ensure t)
+(varan|define-directory "cache" "Directory storing cache files." :ensure t :hidden t)
 (varan|define-directory "statics" "Directory storing static files.")
 (varan|define-directory "templates" "Directory storing template files.")
 (varan|define-directory "user" "Directory storing user data." :ensure t)
