52b6c3606e3a47ebdaeadb2b78cf43c378446f8e
skylerlee <skyler.ac.lee@gmail.com>
2017-07-03 01:11:26 +0800

    Add argument normalize function to varan-general

--- services/varan-general.el
+++ services/varan-general.el
@@ -6,15 +6,27 @@
 (require 'general)
 (require 'use-package)
 
+(defun varan-general//normalize-args (label args)
+  (let (result block head prev)
+    (while args
+      (setq head (pop args)
+            prev (caar block))
+      (when (and (keywordp head) (not (keywordp prev)))
+        (push (nreverse block) result)
+        (setq block nil))
+      (cond
+       ((keywordp head) (push (cons head (pop args)) block))
+       ((consp head) (push head block))
+       (t (varan/error "%s invalid argument: %s" label head))))
+    (push (nreverse block) result)
+    (nreverse result)))
+
 ;; Extend use-package with `:bind-general' keyword
 (add-to-list 'use-package-keywords :bind-general t)
 
 (defun use-package-normalize/:bind-general (name keyword args)
   (use-package-as-one (symbol-name keyword) args
-    (lambda (label arg)
-      (unless (consp arg)
-        (use-package-error (concat label " wants a list of (key . def)")))
-      arg)))
+    'varan-general//normalize-args))
 
 (defun use-package-handler/:bind-general (name keyword arg rest state)
   (use-package-handler/:bind name keyword arg rest state
