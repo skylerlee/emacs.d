c6acf37c7888d8298a9a68cd3b551a9d8ad6d830
skylerlee <skyler.ac.lee@gmail.com>
2017-07-04 23:10:32 +0800

    Remove bind-general arguments normalizer
    This arguments normalizer will cause type error in use-package,
    so it is better to process the arguments in handler.

--- services/varan-general.el
+++ services/varan-general.el
@@ -6,27 +6,11 @@
 (require 'general)
 (require 'use-package)
 
-(defun varan-general//normalize-args (label args)
-  (let (result block head prev)
-    (while args
-      (setq head (pop args)
-            prev (caar block))
-      (when (and (keywordp head) (not (keywordp prev)))
-        (push (nreverse block) result)
-        (setq block nil))
-      (cond
-       ((keywordp head) (push (cons head (pop args)) block))
-       ((consp head) (push head block))
-       (t (varan/error "%s invalid argument: %s" label head))))
-    (push (nreverse block) result)
-    (nreverse result)))
-
 ;; Extend use-package with `:bind-general' keyword
 (add-to-list 'use-package-keywords :bind-general t)
 
 (defun use-package-normalize/:bind-general (name keyword args)
-  (use-package-as-one (symbol-name keyword) args
-    'varan-general//normalize-args))
+  (use-package-normalize-binder name keyword args))
 
 (defun use-package-handler/:bind-general (name keyword arg rest state)
   (use-package-handler/:bind name keyword arg rest state
