0efa1bc74634ddf2a9bfa9af29e9ae14b0f4a054
skylerlee <skyler.ac.lee@gmail.com>
2017-07-06 00:07:25 +0800

    Apply general keybinding to interaction setting

--- settings/interaction.el
+++ settings/interaction.el
@@ -1,8 +1,9 @@
 (use-package ivy
   :diminish ivy-mode
-  :bind (:map ivy-minibuffer-map
-              ("<escape>" . minibuffer-keyboard-quit)
-              ("<right>" . ivy-alt-done))
+  :general
+  (:keymaps 'ivy-minibuffer-map
+            "<escape>" 'minibuffer-keyboard-quit
+            "<right>" 'ivy-alt-done)
   :init
   (setq ivy-re-builders-alist
         '((counsel-M-x . ivy--regex-fuzzy)
@@ -16,9 +17,11 @@
 
 (use-package counsel
   :diminish counsel-mode
-  :bind ("M-x" . counsel-M-x)
+  :general
+  ("M-x" 'counsel-M-x)
   :init
   (add-hook 'after-init-hook 'counsel-mode))
 
 (use-package swiper
-  :bind ("C-s" . swiper))
+  :general
+  ("C-s" 'swiper))
