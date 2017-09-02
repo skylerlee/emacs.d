03907fa1440e8e4bd1b5922be3238c209aada072
skylerlee <skyler.ac.lee@gmail.com>
2017-09-02 22:55:25 +0800

    Update filetree keybindings

--- settings/filetree.el
+++ settings/filetree.el
@@ -3,6 +3,9 @@
   (setq neo-theme 'icons
         neo-smart-open t)
   :general
-  ("<f8>" 'neotree-toggle))
+  ("<f8>" 'neotree-toggle)
+  (:keymaps 'evil-normal-state-map
+            :prefix ","
+            "ft" 'neotree-show))
 
 (use-package all-the-icons)
